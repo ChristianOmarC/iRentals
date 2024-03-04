a05b58fa52a3f53fe128559db0da778789dc4e90
Christian Culajay <culajay.christian@gmail.com>
2024-03-04 15:38:51 -0500

    listing for accounts

--- api/routers/properties.py
+++ api/routers/properties.py
@@ -24,5 +24,11 @@ def create_property(
 
 @router.get("/api/properties", response_model = PropertyList)
 def list_property(repo: PropertiesRepo = Depends()):
-    return PropertyList(properties=repo.get_all())
+    return repo.get_all()
 
+@router.get("/api/properties/own", response_model = PropertyList)
+def list_properties_for_account(
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: PropertiesRepo = Depends()
+    ):
+    return repo.get_all_for_account(account_id=account_data['id'])
