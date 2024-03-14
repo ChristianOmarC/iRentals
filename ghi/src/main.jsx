3e5766041bfcc7f1a5b34af37e7198b4d804fd27
mmilton <murray.l.milton@outlook.com>
2024-03-14 13:56:33 +0100

    made revisions to readme

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -14,17 +14,7 @@ import ListProperties from './components/ListProperties'
 import SignUpForm from './SignupForm'
 import LoginForm from './LoginForm'
 import CreateProperty from './components/CreateProperty'
-<<<<<<< HEAD
-import Dashboard from './components/Dashboard/Dashboard'
-import PropertyDetails from './components/PropertyDetails'
-import UpdateReservation from './components/UpdateReservation'
-import ListReservations from './components/ListReservations'
-import CreateReservation from './components/CreateReservation'
-import ReservationDetail from './components/ReservationDetails'
-import UpdateProperty from './components/UpdateProperty'
-=======
 import Dashboard from './components/Dashboard/DashboardComponents/Dashboard'
->>>>>>> dashpanel
 const router = createBrowserRouter([
     {
         path: '/',
@@ -34,16 +24,16 @@ const router = createBrowserRouter([
             { index: true, element: <Home /> },
             { path: 'about', element: <About /> },
             { path: 'properties', element: <ListProperties /> },
-            { path: 'properties/:id', element: <PropertyDetails /> },
+            // { path: 'properties/:id', element: <PropertyDetails /> },
             { path: 'signup', element: <SignUpForm /> },
             { path: 'login', element: <LoginForm /> },
             { path: 'createproperty', element: <CreateProperty /> },
             { path: 'dashboard', element: <Dashboard /> },
-            { path: 'reservations', element: <ListReservations />},
-            { path: 'reservations/:id', element: <ReservationDetail />},
-            { path: 'createreservation', element: <CreateReservation />},
-            { path: 'reservations/:id/update', element: <UpdateReservation /> },
-            { path: 'properties/:id/update', element: <UpdateProperty />},
+            // { path: 'reservations', element: <ListReservations /> },
+            // { path: 'reservations/:id', element: <ReservationDetail /> },
+            // { path: 'createreservation', element: <CreateReservation /> },
+            // { path: 'reservations/:id/update', element: <UpdateReservation /> },
+            // { path: 'properties/:id/update', element: <UpdateProperty /> },
 
         ],
     },
