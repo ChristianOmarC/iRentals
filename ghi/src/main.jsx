dcf63517b7100bbcdc6b9b583ae55b8e78150699
mmilton <murray.l.milton@outlook.com>
2024-03-18 21:39:32 +0100

    resolved conflict

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -10,9 +10,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import store from './app/store'
 import { Provider } from 'react-redux'
 import ListProperties from './components/ListProperties'
+import ReservationDetail from './components/ReservationDetails'
+import ListReservations from './components/ListReservations'
+import CreateReservation from './components/CreateReservation'
+import UpdateReservation from './components/UpdateReservation'
 import SignUpForm from './SignupForm'
 import LoginForm from './LoginForm'
 import CreateProperty from './components/CreateProperty'
+import UpdateProperty from './components/UpdateProperty'
 import DashApp from './components/Dashboard/DashboardComponents/DashApp'
 const router = createBrowserRouter([
     {
@@ -26,7 +31,7 @@ const router = createBrowserRouter([
             { path: 'signup', element: <SignUpForm /> },
             { path: 'login', element: <LoginForm /> },
             { path: 'createproperty', element: <CreateProperty /> },
-            { path: 'dashboard', element: <Dashboard /> },
+            { path: 'dashboard', element: <DashApp /> },
             { path: 'reservations', element: <ListReservations /> },
             { path: 'reservations/:id', element: <ReservationDetail /> },
             { path: 'createreservation', element: <CreateReservation /> },
