b51caa04e73d9d15d37a4590855dab82617e91ef
Justin Leopard <justinleopard@gmail.com>
2024-03-19 09:37:48 -0400

    Resolving main

--- ghi/src/components/ReservationDetails.jsx
+++ ghi/src/components/ReservationDetails.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { useGetReservationByIdQuery, useDeleteReservationMutation } from '../app/apiSlice'
 import { useParams, Link, useNavigate } from 'react-router-dom'
